7c4fdb6f2e4317c6f34df8adb20054cbbb51011a
Joel Buhrman <joel.buhrman@live.se>
2017-10-19 17:36:03 +0200

    Weird import deleted

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import './styles/styles.css'
-import Scrollchor from 'react-scrollchor'
 import $ from 'jquery'
 
 export default class Header extends Component{
