596a12c328357b12dd1f2da96e7b987a75b8c1d5
Joel Buhrman <joel.buhrman@live.se>
2017-10-20 22:15:52 +0200

    got rid of errors

--- src/components/grades/Average.js
+++ src/components/grades/Average.js
@@ -2,63 +2,71 @@ import React, {Component} from 'react'
 import $ from 'jquery'
 
 let interval = null
+let interval2 = null
 export default class Average extends Component{
 
 
   constructor(){
     super()
     this.state= {
-      value: 0.1,
+      value: 1
     }
 
     this.increaseValue = this.increaseValue.bind(this)
+    this.increaseValueSecond = this.increaseValueSecond.bind(this)
   }
 
-  increaseValue(){
-    if(this.state.value*1.03 >= 4.56){
+  increaseValueSecond(){
+    if(this.state.value*1.005 >= 4.56){
       this.setState({
         value: 4.56
       })
     }
     else{
       this.setState({
-        value: this.state.value *1.03
+        value: this.state.value *1.005
       })
     }
+  }
+
+  increaseValue(){
+    interval2 = setInterval(this.increaseValueSecond, 20)
+    clearInterval(interval)
 
   }
 
 
+
   componentDidMount(){
     let iv= this.increaseValue
-    if(window.location.pathname === '/grades' || window.location.pathname === '/myprojects'){
+    if(window.location.pathname === '/grades' || window.location.pathname === '/myprojects' ){
       interval = setInterval(iv, 20);
     }
     $( "#firstPage" ).scroll(function() {
       let y = $('#firstPage').scrollTop();
       switch (true) {
         case y>10 && !interval:
-          interval = setInterval(iv, 20);
+          interval = setInterval(iv, 100);
           break;
         default:
         break;
       }
     });
+
   }
 
 
   render(){
 
-    if(this.state.value >= 5){
-      clearInterval(interval)
+    if(this.state.value >= 240){
+      clearInterval(interval2)
     }
 
     return(
-      <div className="average rightBorder">
+      <div className="average rightBorder" ref={this.increaseValue}>
         <span className="avg">{this.state.value.toFixed(2)}</span> <br/>
         AVERAGE
       </div>
-
     )
   }
 }
